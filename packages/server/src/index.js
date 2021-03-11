83e18658ed42338a085e42cd7ece742091356405
Rod Vagg <rod@vagg.org>
2021-03-12 01:05:14 +1100

    feat: full deal-list output & custom host & listen config (#2)

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -3,7 +3,9 @@ const { mainnet } = require('@filecoin-shipyard/lotus-client-schema')
 const { NodejsProvider } = require('@filecoin-shipyard/lotus-client-provider-nodejs')
 
 // TODO: Use deployed lotus
-const endpointUrl = 'wss://api.chain.love/rpc/v0'
+const endpointUrl = process.env.ENDPOINT || 'wss://api.chain.love/rpc/v0'
+const port = parseInt(process.env.PORT, 10) || 8001
+const host = process.env.HOST || 'localhost'
 
 const fastify = require('fastify')({
   logger: true
@@ -19,7 +21,7 @@ fastify.post('/lotus', async (request, reply) => {
   const client = new LotusRPC(provider, { schema: mainnet.fullNode })
 
   const deals = await client.clientListDeals()
-  
+
   reply.type('application/json').code(200)
   return deals
 })
@@ -29,7 +31,7 @@ fastify.post('/', async (request, reply) => {
   return request.body
 })
 
-fastify.listen(8001, (err, address) => {
+fastify.listen({ port, host }, (err, address) => {
   if (err) throw err
   fastify.log.info(`server listening on ${address}`)
 })
